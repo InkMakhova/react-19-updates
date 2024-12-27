8fe3a35dd33794c360218f2cb1d51cccfec9b647
Inna Makhova <i.m.makhova@gmail.com>
2024-12-27 17:24:07 +0200

    add fetched data without use state and useEffect via use method

--- src/components/UseExample.js
+++ src/components/UseExample.js
@@ -1,15 +1,11 @@
-import { useState, useEffect } from 'react'
+import { use } from 'react'
+
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
 
 export default function UseExample() {
-  const [users, setUsers] = useState([])
-
-  useEffect(() => {
-    async function fetchUsers() {
-      const response = await fetch('https://jsonplaceholder.typicode.com/users')
-      setUsers(await response.json())
-    }
-    fetchUsers()
-  }, [])
+  // Use "use" method with fetch or context, no need useState, useEffect
+  const users = use(fetchUsers)
 
   return (
     <>
